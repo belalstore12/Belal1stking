4345b98ecad278f64aeb94fbb4483074436a2fd0
belalstore12 <130758757+belalstore12@users.noreply.github.com>
2023-05-13 22:26:34 +0300

    Add files via upload

---
+++ pages/new.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>New App</title>
+</head>
+<body>
+  <div class="page">
+    <div class="navbar">
+      <div class="navbar-inner sliding">
+        <div class="left">
+          <a href="#" class="link back"><span class=
+          "ios-only">Back</span></a>
+        </div>
+        <div class="title">
+          New App
+        </div>
+        <div class="right">
+          <a href="javascript:window.location.reload(true)" class=
+          "f7-icons">reload</a>
+        </div>
+      </div>
+    </div>
+    <div class="page-content">
+      <div class="list media-list inset elevation-12">
+        <ul>
+
+                                </li>
+
+
+
+
+
+تەنها 4 ئەپ ئیش ناکات
+
+<br>
+esgin 🔴
+<br>
+minecraft🔴
+<br>
+gta kurdish🔴
+<br>
+Org 2024🔴
+<br>
+
+
+        </ul>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
